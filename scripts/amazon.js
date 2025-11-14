943754bdf658749eadd5ae4ddb4c3dfd19c8bdf6
ItsAbhiJagtap <itsabhijagtap@gmail.com>
2025-11-14 19:25:34 +0530

    cart changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,7 +72,11 @@ let matchingItem;
             quantity:1
         });
         }
-        
-        console.log(cart);
+
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity +=item.quantity;
+        })
+        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
         });
 });
